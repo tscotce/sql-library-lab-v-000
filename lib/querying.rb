146ea3381ec17cea53f09446fd0a94a6218110f5
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 18:43:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,12 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year  
-  FROM pledges 
-  INNER JOIN projects
-  ON pledges.project_id = projects.id 
-  JOIN users
-  ON pledges.user_id = users.id
-  GROUP BY projects.title
-  ORDER BY projects.title;"
+  FROM books 
+  WHERE series_id = 1
+  ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
