306e366c1f70c9bbb4237632bf5a6458643387c0
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 19:59:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -46,6 +46,8 @@ def select_character_names_and_number_of_books_they_are_in
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id 
+  INNER JOIN books
+  ON series.id = books.series_id
   WHERE characters.species = 'human'
   GROUP BY series.title
   ORDER BY COUNT(characters.species) DESC
