f061ea37d27099da030316369081ab31c29771d8
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 19:14:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM "
+  FROM series
+  INNER JOIN authors"
 end
 
 def select_series_title_with_most_human_characters
