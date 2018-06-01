1d120d91e185b31a304a6ac4b4fd640109302e79
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 20:23:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
   ORDER BY characters.motto ASC
-  LIMIT 1"
+  LIMIT 1;"
 end
 
 
