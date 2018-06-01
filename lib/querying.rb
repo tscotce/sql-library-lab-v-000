0d263ffd7771c66f2a8e61175a6a0ad6cd84ba77
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 18:50:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) AS count
-  FROM characters;"
+  FROM characters
+  ORDER BY count;"
 end
 
 def select_name_and_series_subgenres_of_authors
