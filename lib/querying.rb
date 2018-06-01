daf05eae3c0dacfd6518c0cd55a8c7b2cf18f6e6
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 19:53:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species) AS count
+  "SELECT series.title 
   FROM characters
   INNER JOIN series
   ON characters.series_id = series.id
