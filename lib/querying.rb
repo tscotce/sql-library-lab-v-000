f6803fad74cbdbfe9be163f4a553884bbfa7adb0
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 19:28:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,9 +32,10 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title 
-  FROM series
-  INNER JOIN characters
-  ON series."
+  FROM characters
+  INNER JOIN series
+  ON characters.series_id = series.id
+  WHERE "
 end
 
 def select_character_names_and_number_of_books_they_are_in
