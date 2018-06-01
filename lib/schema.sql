3e9469eab4cd8b60477b6a92f7cdf19699fcc0d6
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 18:34:16 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,3 +2,4 @@ CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_
 CREATE TABLE sub_genre (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name  TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
