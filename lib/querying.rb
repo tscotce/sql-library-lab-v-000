06980662ed11e5ee0a05e157d82c8dcd93665902
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 19:29:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,8 +34,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title 
   FROM characters
   INNER JOIN series
-  ON characters.series_id = series.id;
-  GROUP BY series.title"
+  ON characters.series_id = series.id
+  GROUP BY series.title;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
